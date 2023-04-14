95a348088e64a4647f50080b6870fe4c27c58683
Pranjul120568 <pranjuldwivedi120568@gmail.com>
2023-04-14 22:35:39 +0530

    first one

---
+++ config/db.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+const connectDB = async () => {
+  try {
+    // connect to DB, takes the URL and an object of options
+    const connection = await mongoose.connect(process.env.MONGO_URI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    });
+    //log the connection host
+    console.log(
+      `MongoDB connected: ${connection.connection.host}`.underline.cyan
+    );
+  } catch (error) {
+    console.error(`Error: ${error.message}`.red.underline.bold);
+    //the 1 means it will exit with failure
+    process.exit(1);
+  }
+};
+
+export default connectDB;
